e4837953d5dacf9a76e2069e7008d0b6d5196bd9
richardvinc <vincrichard@live.com>
2026-09-09 14:00:30 +0700

    format using biome

--- app/db/db.ts
+++ app/db/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
 const sqlite = new Database("./app/db/data/sqlite.db", {
-  create: true,
+	create: true,
 });
 export const db = drizzle({ client: sqlite });
 migrate(db, { migrationsFolder: "./drizzle" });
--- app/db/seed.ts
+++ app/db/seed.ts
@@ -3,119 +3,119 @@ import { db } from "./db";
 import * as schema from "./schema";
 
 await db.insert(schema.ingredients).values([
-  {
-    name: "odeng/fishcake",
-  },
-  {
-    name: "sawi putih",
-  },
-  {
-    name: "jamur champignon",
-  },
-  {
-    name: "lean ground beef",
-  },
-  {
-    name: "ikan dori",
-  },
-  {
-    name: "miso",
-  },
+	{
+		name: "odeng/fishcake",
+	},
+	{
+		name: "sawi putih",
+	},
+	{
+		name: "jamur champignon",
+	},
+	{
+		name: "lean ground beef",
+	},
+	{
+		name: "ikan dori",
+	},
+	{
+		name: "miso",
+	},
 ]);
 
 await db.insert(schema.menus).values([
-  {
-    name: "tumis odeng sawi putih",
-  },
-  {
-    name: "mun tahu",
-  },
-  {
-    name: "dori sawi putih",
-  },
+	{
+		name: "tumis odeng sawi putih",
+	},
+	{
+		name: "mun tahu",
+	},
+	{
+		name: "dori sawi putih",
+	},
 ]);
 
 await db.insert(schema.menuSchedules).values([
-  {
-    name: "tumis odeng sawi putih",
-    date: getDateGMT7(),
-    ingredients: [
-      {
-        ingredientName: "odeng",
-        amount: "100 gram",
-      },
-      {
-        ingredientName: "jamur",
-        amount: "100 gram",
-      },
-      {
-        ingredientName: "sawi putih",
-        amount: "200 gram",
-      },
-      {
-        ingredientName: "telur",
-        amount: "2 buah",
-      },
-      {
-        ingredientName: "nasi richard",
-        amount: "150 gram",
-      },
-      {
-        ingredientName: "nasi michelle",
-        amount: "130 gram",
-      },
-    ],
-  },
-  {
-    name: "mun tahu",
-    date: getDateGMT7(2),
-    ingredients: [
-      {
-        ingredientName: "tofu firm",
-        amount: "300 gram",
-      },
-      {
-        ingredientName: "lean ground beef",
-        amount: "sisa di kulkas",
-      },
-      {
-        ingredientName: "jamur champignon",
-        amount: "100 gram",
-      },
-      {
-        ingredientName: "nasi richard",
-        amount: "150 gram",
-      },
-      {
-        ingredientName: "nasi michelle",
-        amount: "130 gram",
-      },
-    ],
-  },
-  {
-    name: "dori sawi putih",
-    date: getDateGMT7(3),
-    ingredients: [
-      {
-        ingredientName: "ikan dori",
-        amount: "500 gram",
-      },
-      {
-        ingredientName: "sawi putih",
-        amount: "200 gram",
-      },
-      {
-        ingredientName: "miso",
-        amount: "2 tbsp",
-      },
-      {
-        ingredientName: "nasi richard",
-        amount: "150 gram",
-      },
-      {
-        ingredientName: "nasi michelle",
-        amount: "130 gram",
-      },
-    ],
-  },
+	{
+		name: "tumis odeng sawi putih",
+		date: getDateGMT7(),
+		ingredients: [
+			{
+				ingredientName: "odeng",
+				amount: "100 gram",
+			},
+			{
+				ingredientName: "jamur",
+				amount: "100 gram",
+			},
+			{
+				ingredientName: "sawi putih",
+				amount: "200 gram",
+			},
+			{
+				ingredientName: "telur",
+				amount: "2 buah",
+			},
+			{
+				ingredientName: "nasi richard",
+				amount: "150 gram",
+			},
+			{
+				ingredientName: "nasi michelle",
+				amount: "130 gram",
+			},
+		],
+	},
+	{
+		name: "mun tahu",
+		date: getDateGMT7(2),
+		ingredients: [
+			{
+				ingredientName: "tofu firm",
+				amount: "300 gram",
+			},
+			{
+				ingredientName: "lean ground beef",
+				amount: "sisa di kulkas",
+			},
+			{
+				ingredientName: "jamur champignon",
+				amount: "100 gram",
+			},
+			{
+				ingredientName: "nasi richard",
+				amount: "150 gram",
+			},
+			{
+				ingredientName: "nasi michelle",
+				amount: "130 gram",
+			},
+		],
+	},
+	{
+		name: "dori sawi putih",
+		date: getDateGMT7(3),
+		ingredients: [
+			{
+				ingredientName: "ikan dori",
+				amount: "500 gram",
+			},
+			{
+				ingredientName: "sawi putih",
+				amount: "200 gram",
+			},
+			{
+				ingredientName: "miso",
+				amount: "2 tbsp",
+			},
+			{
+				ingredientName: "nasi richard",
+				amount: "150 gram",
+			},
+			{
+				ingredientName: "nasi michelle",
+				amount: "130 gram",
+			},
+		],
+	},
 ]);
